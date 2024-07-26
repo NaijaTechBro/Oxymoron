17939d2db410b61f6b68ae0f5df07df7f09f8a28
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2024-07-26 03:25:31 +0100

    code refactor

--- .ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-
-res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-soup = BeautifulSoup(res.content,'lxml')
-table = soup.find_all('table')[0] 
-df = pd.read_html(str(table))
-print(df[0].to_json(orient='records'))
