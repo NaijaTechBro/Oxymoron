d3d9a13323fbaffff30236fe0a27748516d397f3
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2024-07-26 03:25:51 +0100

    implementing random Alpha

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import yfinance
-import threading
 import pytz
+import requests
+import threading
+import pandas as pd
+import yfinance  # type: ignore
+import threading
 from datetime import datetime
+from bs4 import BeautifulSoup
 from io import StringIO
-from utils import load_pickle, save_pickle, Alpha
 
 def get_sp500_tickers():
-    res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies") #"https://ngxgroup.com/exchange/trade/equities/listed-companies/
     soup = BeautifulSoup(res.content, 'html.parser')
     table = soup.find_all('table')[0]
-    df = pd.read_html(StringIO(str(table)))[0] 
+    df = pd.read_html(StringIO(str(table)))
     tickers = list(df[0].Symbol)
     return tickers
 
@@ -27,20 +27,20 @@ def get_history(ticker, period_start, period_end, granularity="1d", tries=0):
     except Exception as err:
         if tries < 5:
             return get_history(ticker, period_start, period_end, granularity, tries+1)
-        return pd.DataFrame()
-
+        return pd.DataFrame
+    
     df = df.rename(columns={
-        "Date": "datetime",
-        "Open": "open",
-        "High": "high",
-        "Low": "low",
-        "Close": "close",
-        "Volume": "volume",
+        "Date":"datetime",
+        "Open":"open",
+        "High":"high",
+        "Low":"low",
+        "Close":"close",
+        "Volume":"volume"
     })
     if df.empty:
         return pd.DataFrame()
-
-    df["datetime"] = df["datetime"].dt.tz_localize(pytz.utc)
+    
+    df["datetime"] = df["datetime"].dt.tz_convert(pytz.utc)
     df = df.drop(columns=["Dividends", "Stock Splits"])
     df = df.set_index("datetime", drop=True)
     return df
@@ -49,38 +49,43 @@ def get_histories(tickers, period_starts, period_ends, granularity="1d"):
     dfs = [None]*len(tickers)
     def _helper(i):
         print(tickers[i])
-        df = get_history(
+        df= get_history(
             tickers[i],
-            period_starts[i], 
+            period_starts[i],
             period_ends[i], 
             granularity=granularity
         )
         dfs[i] = df
-    threads = [threading.Thread(target=_helper,args=(i,)) for i in range(len(tickers))]
+    threads = [threading.Thread(target=_helper, args=(i,)) for i in range(len(tickers))]
     [thread.start() for thread in threads]
     [thread.join() for thread in threads]
-    tickers = [tickers[i] for i in range(len(tickers)) if not dfs[i].empty]
+    tickers = [tickers[i] for  i in range(len(tickers)) if not dfs[i].empty]
     dfs = [df for df in dfs if not df.empty]
     return tickers, dfs
 
 def get_ticker_dfs(start, end):
+    from utils import load_pickle, save_pickle
     try:
         tickers, ticker_dfs = load_pickle("dataset.obj")
     except Exception as err:
         tickers = get_sp500_tickers()
-        starts=[start]*len(tickers)
-        ends=[end]*len(tickers)
-        tickers,dfs = get_histories(tickers,starts,ends,granularity="1d")
+        starts = [start]*len(tickers)
+        ends = [end]*len(tickers)
+        tickers, dfs = get_histories(tickers, starts, ends, granularity="1d")
         ticker_dfs = {ticker:df for ticker,df in zip(tickers,dfs)}
         save_pickle("dataset.obj", (tickers, ticker_dfs))
     return tickers, ticker_dfs
-
+    
+from utils import Alpha
 period_start = datetime(2010,1,1, tzinfo=pytz.utc)
 period_end = datetime.now(pytz.utc)
-tickers, ticker_dfs = get_ticker_dfs(start=period_start,end=period_end)
+tickers, ticker_dfs = get_ticker_dfs(start = period_start, end = period_end)
 testfor = 20
-tickers = tickers[:testfor]
-
-alpha = Alpha(insts=tickers, dfs=ticker_dfs,start=period_start,end=period_end)
+tickers =tickers[:testfor]
+alpha = Alpha(
+    insts=tickers, 
+    dfs=ticker_dfs, 
+    start=period_start, 
+    end=period_end)
 portfolio_df = alpha.run_simulation()
 print(portfolio_df)
--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 import lzma
-import dill as pickle
-import pandas as pd
 import numpy as np
-import random
+import pandas as pd
+import dill as pickle # type: ignore
+from datetime import timedelta
 
 def load_pickle(path):
-    with lzma.open(path, "rb") as fp:
+    with lzma.open(path,"rb") as fp:
         file = pickle.load(fp)
     return file
 
-def save_pickle(path, obj):
-    with lzma.open(path, "wb") as fp:
+def save_pickle(path,obj):
+    with lzma.open(path,"wb") as fp:
         pickle.dump(obj,fp)
-        
+
 def get_pnl_stats(date, prev, portfolio_df, insts, idx, dfs):
     day_pnl = 0
     nominal_ret = 0
@@ -31,33 +31,35 @@ def get_pnl_stats(date, prev, portfolio_df, insts, idx, dfs):
     return day_pnl, capital_ret
 
 class Alpha():
-
+    
     def __init__(self, insts, dfs, start, end):
         self.insts = insts
         self.dfs = dfs
-        self.start = start
+        self.start = start 
         self.end = end
 
     def init_portfolio_settings(self, trade_range):
         portfolio_df = pd.DataFrame(index=trade_range)\
             .reset_index()\
             .rename(columns={"index":"datetime"})
-        portfolio_df.loc[0, "capital"] = 10000
+        portfolio_df.loc[0,"capital"] = 10000
         return portfolio_df
 
     def compute_meta_info(self,trade_range):
         for inst in self.insts:
-            df=pd.DataFrame(index=trade_range)
+            df = pd.DataFrame(index=trade_range)
             self.dfs[inst] = df.join(self.dfs[inst]).ffill().bfill()
-            self.dfs[inst]["ret"] = -1 + self.dfs[inst]["close"]/self.dfs[inst]["close"].shift(1)
+            self.dfs[inst]["ret"] = -1 + self.dfs[inst]["close"] / self.dfs[inst]["close"].shift(1)
             sampled = self.dfs[inst]["close"] != self.dfs[inst]["close"].shift(1).bfill()
             eligible = sampled.rolling(5).apply(lambda x: int(np.any(x))).fillna(0)
             self.dfs[inst]["eligible"] = eligible.astype(int) & (self.dfs[inst]["close"] > 0).astype(int)
-            input(self.dfs[inst])
         return 
+
     def run_simulation(self):
         print("running backtest")
-        date_range = pd.date_range(start=self.start,end=self.end, freq="D")
+        start = self.start +timedelta(hours=5)
+        end = self.end+timedelta(hours=5)
+        date_range = pd.date_range(start, end, freq="D")
         self.compute_meta_info(trade_range=date_range)
         portfolio_df = self.init_portfolio_settings(trade_range=date_range)
         for i in portfolio_df.index:
@@ -78,6 +80,7 @@ class Alpha():
                 )
             
             alpha_scores = {}
+            import random
             for inst in eligibles:
                 alpha_scores[inst] = random.uniform(0,1)
             alpha_scores = {k:v for k,v in sorted(alpha_scores.items(),key=lambda pair:pair[1])}
