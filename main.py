abfb1ec30b0b6c30d4c274b94c39edb116084473
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2024-07-26 04:30:21 +0100

    update main

--- main.py
+++ main.py
@@ -88,11 +88,11 @@ tickers, ticker_dfs = get_ticker_dfs(start = period_start, end = period_end)
 testfor = 20
 tickers =tickers[:testfor]
 
-from alpha1 import Alpha1
-alpha1 = Alpha1(
+from alpha2 import Alpha2
+alpha2 = Alpha2(
     insts=tickers, 
     dfs=ticker_dfs, 
     start=period_start, 
     end=period_end)
-df1 = alpha1.run_simulation()
+df1 = alpha2.run_simulation()
 print(df1)
