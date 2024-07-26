1108c4b76cc626baa11f08a33b6a9d67a68a6a95
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2024-07-26 03:33:05 +0100

    formatted to add comment

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from io import StringIO
 
+# Get all tickers (web scrapping)
 def get_sp500_tickers():
     res = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies") #"https://ngxgroup.com/exchange/trade/equities/listed-companies/
     soup = BeautifulSoup(res.content, 'html.parser')
@@ -16,6 +17,7 @@ def get_sp500_tickers():
     tickers = list(df[0].Symbol)
     return tickers
 
+# get formatted history
 def get_history(ticker, period_start, period_end, granularity="1d", tries=0):
     try:
         df = yfinance.Ticker(ticker).history(
@@ -45,6 +47,7 @@ def get_history(ticker, period_start, period_end, granularity="1d", tries=0):
     df = df.set_index("datetime", drop=True)
     return df
 
+# get histories for all tickers
 def get_histories(tickers, period_starts, period_ends, granularity="1d"):
     dfs = [None]*len(tickers)
     def _helper(i):
@@ -56,6 +59,7 @@ def get_histories(tickers, period_starts, period_ends, granularity="1d"):
             granularity=granularity
         )
         dfs[i] = df
+        # threading in other to get the len list of all the tickers
     threads = [threading.Thread(target=_helper, args=(i,)) for i in range(len(tickers))]
     [thread.start() for thread in threads]
     [thread.join() for thread in threads]
@@ -63,6 +67,7 @@ def get_histories(tickers, period_starts, period_ends, granularity="1d"):
     dfs = [df for df in dfs if not df.empty]
     return tickers, dfs
 
+# get tickers in the data frame after consuming data from yfinance (save in a dataset object)
 def get_ticker_dfs(start, end):
     from utils import load_pickle, save_pickle
     try:
@@ -75,7 +80,7 @@ def get_ticker_dfs(start, end):
         ticker_dfs = {ticker:df for ticker,df in zip(tickers,dfs)}
         save_pickle("dataset.obj", (tickers, ticker_dfs))
     return tickers, ticker_dfs
-    
+    # Alpha Backtest Usecase
 from utils import Alpha
 period_start = datetime(2010,1,1, tzinfo=pytz.utc)
 period_end = datetime.now(pytz.utc)
