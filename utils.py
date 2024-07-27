5eac62e04482ae62efb59135f94ac86340ad22f9
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2024-07-27 05:33:05 +0100

    update param and cleaned code

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import lzma
 import numpy as np
+import random
 import pandas as pd
 import dill as pickle # type: ignore
 from datetime import timedelta
+from copy import deepcopy
 
 def load_pickle(path):
     with lzma.open(path,"rb") as fp:
@@ -30,11 +32,13 @@ def get_pnl_stats(date, prev, portfolio_df, insts, idx, dfs):
     portfolio_df.loc[idx,"capital_ret"] = capital_ret
     return day_pnl, capital_ret
 
+class AbstractImplementationException(Exception):
+    pass
 class Alpha():
     
     def __init__(self, insts, dfs, start, end):
         self.insts = insts
-        self.dfs = dfs
+        self.dfs = deepcopy(dfs)
         self.start = start 
         self.end = end
 
@@ -44,8 +48,19 @@ class Alpha():
             .rename(columns={"index":"datetime"})
         portfolio_df.loc[0,"capital"] = 10000
         return portfolio_df
-
+    
+    def pre_compute(self, trade_range):
+        pass
+        
+    def post_compute(self, trade_range):
+        pass
+    
+    def compute_signal_distribution(self, eligible, date):
+        raise AbstractImplementationException("no concrete implementation for signal generation")
+    
     def compute_meta_info(self,trade_range):
+        self.pre_compute(trade_range=trade_range)
+        
         for inst in self.insts:
             df = pd.DataFrame(index=trade_range)
             self.dfs[inst] = df.join(self.dfs[inst]).ffill().bfill()
@@ -53,10 +68,11 @@ class Alpha():
             sampled = self.dfs[inst]["close"] != self.dfs[inst]["close"].shift(1).bfill()
             eligible = sampled.rolling(5).apply(lambda x: int(np.any(x))).fillna(0)
             self.dfs[inst]["eligible"] = eligible.astype(int) & (self.dfs[inst]["close"] > 0).astype(int)
+            
+        self.post_compute(trade_range=trade_range)
         return 
-
+    
     def run_simulation(self):
-        print("running backtest")
         start = self.start +timedelta(hours=5)
         end = self.end+timedelta(hours=5)
         date_range = pd.date_range(start, end, freq="D")
@@ -78,9 +94,9 @@ class Alpha():
                     idx=i,
                     dfs=self.dfs
                 )
+            forecasts, forecast_chips = self.compute_signal_distribution(eligibles, date)
             
             alpha_scores = {}
-            import random
             for inst in eligibles:
                 alpha_scores[inst] = random.uniform(0,1)
             alpha_scores = {k:v for k,v in sorted(alpha_scores.items(),key=lambda pair:pair[1])}
@@ -93,8 +109,8 @@ class Alpha():
             
             nominal_tot = 0
             for inst in eligibles:
-                forecast = 1 if inst in alpha_long else (-1 if inst in alpha_short else 0)
-                dollar_allocation = portfolio_df.loc[i,"capital"] / (len(alpha_long) + len(alpha_short))
+                forecast = forecasts[inst]
+                dollar_allocation = portfolio_df.loc[i,"capital"] / forecast_chips if forecast_chips !=0 else 0
                 position = forecast * dollar_allocation / self.dfs[inst].loc[date,"close"]
                 portfolio_df.loc[i, inst + " units"] = position 
                 nominal_tot += abs(position * self.dfs[inst].loc[date,"close"])
